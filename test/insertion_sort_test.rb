da1365f1cb6246b5e93c4af080a79d1ba56d1b35
Erik Nilsen <enilsen16@live.com>
2014-06-10 13:47:46 -0700

    fix and add more tests

--- test/insertion_sort_test.rb
+++ test/insertion_sort_test.rb
@@ -7,4 +7,16 @@ describe "insertion sort" do
     a.sort_array
     a.must_equal [1,3,4,5,8]
   end
+
+  it "will sort a reverse array" do
+    a = [5, 4, 3, 2, 1]
+    a.sort_array
+    a.must_equal [1,2,3,4,5]
+  end
+
+  it "will sort the best case" do
+    a = [1,2,3,4,5]
+    a.sort_array
+    a.must_equal [1,2,3,4,5]
+  end
 end
