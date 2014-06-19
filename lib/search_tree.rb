45c2f4ca90d4f39c5e4a90fe797ef9d8aedeb6ce
Erik Nilsen <enilsen16@live.com>
2014-06-19 13:13:17 -0700

    search tree

--- lib/search_tree.rb
+++ lib/search_tree.rb
@@ -16,24 +16,38 @@ class Search_tree
 
   def size
     if @count == 0
-      return nil
+      return 0
     else
       return @count
     end
   end
 
-  def depth
+  def depth(node = @root)
+    return 1 if node.children.empty?
+    node.children.map { |x| 1 + depth(x) }.max
   end
 
-  def balance
+  def balance(node = @root)
+    return 0 if node.children.empty?
+    if @root.left.nil?
+      depth_left = 0
+      depth_right = depth(node.right)
+    elsif @root.right.nil?
+      depth_right = 0
+      depth_left = depth(node.left)
+    else
+      depth_left = depth(node.left)
+      depth_right = depth(node.right)
+    end
+    depth_left - depth_right
   end
 
 private
 
   def to_insert(top_node,value_to_insert)
     if top_node.nil?
-      Node_tree.new(value_to_insert)
       @count += 1
+      return Node_tree.new(value_to_insert)
     elsif top_node == value_to_insert
       top_node.value = value_to_insert
     elsif value_to_insert < top_node.value
@@ -57,7 +71,6 @@ private
       top_node = search_for(top_node.right, new_node)
     end
       return top_node
-      binding.pry
   end
 end
 
@@ -68,5 +81,9 @@ class Node_tree
     @value = value
     @left = left
     @right = right
+  end
+
+  def children
+    [@left, @right].compact
   end
 end
--- test/search_tree_test.rb
+++ test/search_tree_test.rb
@@ -5,19 +5,43 @@ describe "A Binary Search Tree" do
   it "will insert a root node" do
     search_tree = Search_tree.new
     node4 = search_tree.insert(4)
-    search_tree.root.contain(4).must_equal true
+    search_tree.contain(4).must_equal true
   end
 
   it "will add a node to the head" do
     search_tree = Search_tree.new
     node4 = search_tree.insert(4)
     search_tree.insert(5)
-    node4.contain(5).must_equal true
+    search_tree.contain(5).must_equal true
     search_tree.size.must_equal 2
   end
 
   it "will return nil if the tree doesnt contain any values" do
     search_tree = Search_tree.new
-    search_tree.size.must_equal nil
+    search_tree.size.must_equal 0
+  end
+
+  it "will return the proper depth" do
+    search_tree = Search_tree.new
+    search_tree.insert(4)
+    search_tree.insert(5)
+    search_tree.contain(5).must_equal true
+    search_tree.size.must_equal 2
+    search_tree.depth.must_equal 2
+  end
+
+  it "will return 0 if the Search Tree is only a root" do
+    search_tree = Search_tree.new
+    node4 = search_tree.insert(4)
+    search_tree.contain(4).must_equal true
+    search_tree.balance.must_equal 0
+  end
+
+  it "will return 1 if the left is bigger than the right" do
+    search_tree = Search_tree.new
+    search_tree.insert(5)
+    search_tree.insert(4)
+    search_tree.contain(4).must_equal true
+    search_tree.balance.must_equal 1
   end
 end
