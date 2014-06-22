d3237afb76d86a1c93dbb3e2bb564bc30a554dbd
Erik Nilsen <enilsen16@live.com>
2014-06-22 13:44:44 -0700

    add benchmarking to quicksort

--- test/quick_sort_test.rb
+++ test/quick_sort_test.rb
@@ -1,5 +1,6 @@
 require "minitest/autorun"
 require "quick_sort"
+require "benchmark"
 
 describe "quick sort" do
   it "will sort a given array" do
@@ -16,4 +17,14 @@ describe "quick sort" do
     a = [1, 2, 3, 4, 5]
     a.quicksort.must_equal [1, 2, 3, 4, 5]
   end
+
+  @best_case = (1..10000).to_a
+  @reverse_array = @best_case.reverse
+  @random_array = (1..10000).to_a.shuffle
+  puts "best case Quick Sort"
+  puts Benchmark.measure{ @best_case.quicksort }
+  puts "reverse Quick Sort"
+  puts Benchmark.measure{ @reverse_array.quicksort}
+  puts "random Quick Sort"
+  puts Benchmark.measure{ @random_array.quicksort }
 end
